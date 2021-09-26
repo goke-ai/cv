7efdf23f3f2d57be947d90199d2f200450641b76
Olagoke Oladokun <gokeladokun@hotmail.com>
2021-09-26 09:56:43 +0100

    Add chapter 10, 11, 12, 13

---
+++ essential/test_command_line.py
@@ -0,0 +1,33 @@
+import sys
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+print("commandline argument array \n", sys.argv)
+print()
+print("first argument: ", sys.argv[0])
+print("second argument: ", sys.argv[1])
+print("third argument: ", sys.argv[2])
+
+
+filePath = sys.argv[1]
+
+# read image file
+img = cv.imread(filePath)
+
+# check if file found
+if img is None:
+    sys.exit("Could not read the image.")
+
+# display file
+cv.imshow("Display window", img)
+
+# pause execution here by waiting for a user to press a key
+k = cv.waitKey(0)
+
+# if user typed s save file as PNG file
+if k == ord("s"):
+    cv.imwrite("test.png", img)
+
+cv.destroyAllWindows()
+
+
