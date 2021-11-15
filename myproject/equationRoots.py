d569805e9ff67da24a879369b20ec51e213bc545
Olagoke Oladokun <gokeladokun@hotmail.com>
2021-11-15 08:02:32 +0100

    Add a21

---
+++ myproject/equationRoots.py
@@ -0,0 +1,18 @@
+
+
+# define variables
+a = 13
+b = 1
+c = -13
+
+# formulae equation
+# dicriminate
+D = b**2 - 4*a*c
+
+# compute roots
+x1 = (-b + (D)**.5) / (2*a)
+x2 = (-b - (D)**.5) / (2*a)
+
+# show results
+print(f"x1 = {x1:4f}")
+print(f"x2 = {x2:4f}")
