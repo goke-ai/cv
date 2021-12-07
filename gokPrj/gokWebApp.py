c0286b303b857a9be145c92dd369dd87ad4244fd
Olagoke Oladokun <gokeladokun@hotmail.com>
2021-12-07 06:12:36 +0100

    Add index route

--- gokPrj/gokWebApp.py
+++ gokPrj/gokWebApp.py
@@ -1,10 +1,21 @@
 from flask import Flask
 
+# instance of Flask
 app = Flask(__name__)
 
+# default route
 @app.route("/")
+def index():
+    return "Welcome! to Index Page"
+
+@app.route("/hello")
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
+# /user
+@app.route("/user/<username>")
+def hello_user(username):
+    return f"Welcome!, {username}"
 
 
 if __name__ == "__main__":
