1e7d4386f378842f9d764a9049b4dd5123628fe8
Olagoke Oladokun <gokeladokun@hotmail.com>
2021-12-07 06:02:25 +0100

    Add 38

---
+++ gokPrj/helloWorld.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ gokPrj/tensorflow_vs_pytorch.py
@@ -0,0 +1,34 @@
+# Workout 
+# Submit max models before 7pm
+
+
+
+# using thesame parameters
+# epoch = 30
+# maxIteration = 5
+# save file name ending with suffix of accuracy.
+# 
+
+
+
+# extract the code for simulating tensorflow
+# separate module (cv_image_tensorflow_model.py)
+
+
+
+# extract the code for simulating pytorch
+# separate module (cv_image_pytorch_model.py)
+
+
+
+
+# load the saved models with the highest accuracy for tensorflow and pytorch
+# and predict for same set of dataImages
+# (cv_image_tensorflow_vs_pytorch.py)
+
+
+
+
+# Get your project dataset ready for presentation tomorrow.
+
+
